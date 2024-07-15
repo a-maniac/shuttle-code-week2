a5cfc556dc7b2241fc6342a148d2feb7c74457f0
amanjoshi <ajoshi373@gmail.com>
2024-07-15 14:16:55 +0530

    2.7: ApiResponse and GlobalResponseHandler

---
+++ src/main/java/com/example/SpringMVC/advices/ApiResponse.java
@@ -0,0 +1,28 @@
+package com.example.SpringMVC.advices;
+
+import lombok.Data;
+import org.modelmapper.internal.bytebuddy.asm.Advice;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ApiResponse<T> {
+
+    private T data;
+    private ApiError error;
+    private LocalDateTime timestamp;
+
+    public ApiResponse(T data) {
+        this(); // without default constructor the timestamp won't be visible
+        this.data = data;
+    }
+
+    public ApiResponse(ApiError error) {
+        this();
+        this.error = error;
+    }
+
+    public ApiResponse() {
+        this.timestamp = LocalDateTime.now();
+    }
+}
